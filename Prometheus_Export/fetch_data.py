0cf3f8f1fc858452d5cbdb4a07a0db4b555b4bd1
longtinTam <langtit@uci.edu>
2021-09-11 23:23:34 -0700

    corre and fetch traces

--- Prometheus_Export/fetch_data.py
+++ Prometheus_Export/fetch_data.py
@@ -1,95 +1,139 @@
-import csv
+from os import stat_result
+import pandas as pd
+from pandas.core.indexes.base import Index
 import requests
 import sys
-
+import os.path
 import datetime
 import argparse
-def GetMetrixNames(url):
+
+# For all the fields in this list, create a separate column
+# Example: node_disk_discard_time_seconds_total{device="sda"} -> node_disk_discard_time_seconds_total__device_sda
+split_list = [ 'mode' ,  'device', ]
+
+# For all the fields in this list, create an average of all values for each timestamp
+# Example: node_cpu_frequency_max_hertz{cpu="0"} + {cpu="1"} + .. {cpu="5"} / 6
+avg_list = [ 'cpu' ]
+
+
+parser = argparse.ArgumentParser(description='Get data from Prometheus Server')
+
+def getLabelNames(url):
     response = requests.get('{0}/api/v1/label/__name__/values'.format(url))
-    names = response.json()['data']    #Return metrix names
+    print("Fetching timeseries keys")
+    # print(response.request.url)
+    names = response.json()['data']    #Return matrix names
     return names
 
-print(sys.argv[2])
-# added 
-parser = argparse.ArgumentParser(description='N/A')
-parser.add_argument('-l', '--url',
-                    action='store',
-                    type=str,
-                    )
-parser.add_argument('-f', '--filename',
-                    action='store',
-                    type=str,
-                    )
-parser.add_argument('-n', '--namespace',
-                    action='store',
-                    type=str,
-                    default='default',
-                    )
-parser.add_argument('--interval',
-                    action='store',
-                    type=str,
-                    default='5m',
-                    )
-parser.add_argument('--start',
-                    action='store',
-                    type=str,
-                    )
-parser.add_argument('--end',
-                    action='store',
-                    type=str,
-                   )
-parser.add_argument('--step',
-                    action='store',
-                    type=int,
-                    )
-args = parser.parse_args()
-filepath = args.filename
-namespace = args.namespace
-interval = args.interval
-step = args.step
+def getArguments():
+    parser.add_argument('-l', '--url',
+                        action='store',
+                        type=str,
+                        )
+    parser.add_argument('-f', '--filename',
+                        action='store',
+                        type=str,
+                        )
+    parser.add_argument('--step',
+                        action='store',
+                        type=int,
+                        )
+    args = parser.parse_args()
+    return args
 
-start_str = args.start
-end_str = args.end
-start_dt = datetime.datetime.strptime(start_str, '%Y%m%d-%H%M')
-end_dt = datetime.datetime.strptime(end_str, '%Y%m%d-%H%M')
-start_unix = start_dt.timestamp()
-end_unix = end_dt.timestamp()
+def writeToCSV(args):
+    my_file = open("traces_test.txt","r")
+    my_traces = my_file.readlines()
 
-with open('data.csv','w',newline='') as csvFile:
-    writer = csv.writer(csvFile)
-# writer = csv.writer(sys.stdout)
+    for i in range(len(my_traces)):
+        start = my_traces[i].find("from=")+5
+        mid = my_traces[i].find("&to=")
+        end = my_traces[i].find("\n")
 
+        start_unix = my_traces[i][start:mid-3]
+        end_unix   = my_traces[i][mid+4:end-3]
+        merged_data = pd.DataFrame()
+        print(start_unix)
+        print(end_unix)
+        # metricNames = ["node_cpu_frequency_max_hertz", "node_cpu_guest_seconds_total"]
+        # metricNames = ["node_tplink_power","node_rapl_core_joules_total", "network_address_assign_type"]
+        # metricNames = ["node_rapl_core_joules_total","node_cpu_scaling_frequency_hertz"]
+        
+        metricNames=getLabelNames(args.url)
+
+        for metricName in metricNames:
+            # *****************************************************
+            response = requests.get('{0}/api/v1/query_range'.format(args.url), params={'query': metricName,'start': start_unix, 'end': end_unix,'step': args.step})
+            # print(response.request.url)
+            flag = 0
+            results = response.json()['data']['result']
+            
+            # # Special case 2: Split
+            
+            for metric in split_list:
+                # print(results[0])
+                if (metric in results[0]['metric']):
+                    for i in range(len(results)):
+                        results[i]['metric']['__name__']= str(results[i]['metric']['__name__'])+'_'+metric+'_'+str(results[i]['metric'][metric])
+            #  # Special case 1: Average
+            for metric in avg_list:
+                if (metric in results[0]['metric']):
+                
+                    # df_per_cpu[len(results)] -> Each result is a pandas dataframe
+                    # df_average = avg(df_per_cpu) -> Take average by timestamp
+
+                    average_df = pd.DataFrame()
+                    for k in range(int(len(results)/12)):
+                        avg_df_list=[]
+                        for j in range(len(results[0]['values'])):
+                            sum,avg = 0, 0
+                            for i in range(len(results)):
+                                sum += float(results[i]['values'][j][1])
+                            avg = sum/(len(results))
+                            avg_df_list.append(avg)
+                            # print(avg_df_list)
+                            col_name = str(results[k]['metric']['__name__'])
+                        merged_data[col_name] = avg_df_list
+                    flag = 1
+                        
+                    # frames = [merged_data,average_df]
+                    # result = pd.concat(frames)    
+            #         # ******************************
+                    # print("!!", metric)
+            #         # ******************************
+            for timeseries in results:
+                if flag ==1:
+                    break
+                if 'Time' not in merged_data:
+                    df_time = [value[0] for value in timeseries['values']]
+                    merged_data['Time'] = df_time
+                
+            for timeseries in results:
+                metric_name = timeseries['metric']['__name__']
+                df_value = [value[1] for value in timeseries['values']]
+                if flag ==1:
+                    break
+            
+                if ( len(merged_data['Time']) == len(df_value)):
+                    merged_data[metric_name] = df_value
+                else:
+                    print("[Warning] ", metric_name, " skipped.")
+            # *****************************************************
+        if(os.path.isfile(args.filename)):
+            merged_data.to_csv(args.filename, mode='a',header=False,index=False)
+        else:
+            merged_data.to_csv(args.filename,index=False)
+        # merged_data.to_csv(args.filename)
+        print("-----------------------------------------------------------------------------------------------------------------------------")
+
+def main():
+    args = getArguments()
+ 
     if len(sys.argv) < 2:
-        print('Usage: {0} http://localhost:9090'.format(sys.argv[0]))
+        parser.print_help()
         sys.exit(1)
-    metrixNames=GetMetrixNames(sys.argv[2])
+    
+    writeToCSV(args)
 
-    writeHeader=True
-    for metrixName in metrixNames:
-
-        #now its hardcoded for hourly
-        # response = requests.get('{0}/api/v1/query'.format(sys.argv[2]), params = {'start': start_unix, 'end': end_unix,'step': step})
-        response = requests.get('{0}/api/v1/query'.format(sys.argv[2]), params={'query': metrixName,'start': start_unix, 'end': end_unix,'step': step})
-        results = response.json()['data']['result']
-        # Build a list of all labelnames used.
-        #gets all keys and discard __name__
-        labelnames = set()
-        for result in results:
-            labelnames.update(result['metric'].keys())
-        # Canonicalize
-        labelnames.discard('__name__')
-        labelnames = sorted(labelnames)
-        # Write the samples.
-        if writeHeader:
-        #   writer.writerow(['name', 'timestamp', 'value'] + labelnames)
-            writeHeader=False
-        for result in results:
-            l = [result['metric'].get('__name__', '')] + result['values']
-            for label in labelnames:
-                l.append(result['metric'].get(label, ''))
-                writer.writerow(l)
-    csvFile.close()
-
-
-# -------------------------------------------------------------------------------------------------------------------------
-
+if __name__ == "__main__":
+    main()
--- Prometheus_Export/print_corre.py
+++ Prometheus_Export/print_corre.py
@@ -1,39 +1,42 @@
+from types import coroutine
 from typing import Collection
 from numpy import mean
 from numpy import std
 from numpy.core.fromnumeric import size
 from numpy.random import randn
 from numpy.random import seed
-from matplotlib import pyplot
 import pandas as pd
 from pandas.core.algorithms import value_counts
-import seaborn as sn
+from pandas.io.pytables import Fixed
 
-parameter_list = ["node_tplink_power","node_rapl_core_joules_total","node_rapl_dram_joules_total","go_memstats_mspan_inuse_bytes"]
-data_label = ["tplink_power","core_joules","dram_joules","mspan_inuse_bytes"]
-count, j,k=0,0,0
-data_merge=[]
+fixed_var = ["node_tplink_power","node_rapl_core_joules_total","node_rapl_dram_joules_total","node_rapl_package_joules_total"]
+# fixed_var = ["node_rapl_core_joules_total"]
+threhold = 0
+def find_corr():
+    
+    df = pd.read_csv('data.csv')
+    df = df.drop([df.columns[0],"Time"], axis = 1)
+    corr_matrix = df.corr()
+    filterred_corr_matrix = corr_matrix.filter(items=fixed_var, axis=0).fillna(0)
+    corr_metrics = []
+    for key in filterred_corr_matrix.columns:
+        
+        if key in fixed_var:
+            continue
+        if key.startswith('go'):
+            continue
+        
+        isCorr = True
+        for row in fixed_var:
+            if filterred_corr_matrix[key][row] < threhold:
+                isCorr = False
+                break
+        if (isCorr):
+            print(filterred_corr_matrix[key])
+            corr_metrics.append(key)
+    # print (corr_metrics)
+def main():
+    find_corr()
 
-with open('data.csv','r') as inputfile:
-    rows = [line.split(',') for line in inputfile.readlines()]
-    
-    data = [line[1:-1] for line in rows if line[0] == parameter_list[0]]
-    
-    hashmap={}
-    for i in range(0,4):
-        data = [line[1:-1] for line in rows if line[0] == parameter_list[i]]
-        j=0
-        data_merge=[]
-        while j < len(data[0]):
-            
-            value=data[0][j+1].replace(']"','').replace("'",'"').replace(" ","").replace('"','')
-            data_merge.append(value)
-            j+=2
-        hashmap[data_label[k]] = data_merge
-        k += 1
-        df = pd.DataFrame(data=hashmap,columns=data_label)
-df_flo=df.astype(float)
-corr_matrix = df_flo.corr()
-print(corr_matrix)
-sn.heatmap(corr_matrix, annot=True)
-pyplot.show()
+if __name__ == "__main__":
+    main()
